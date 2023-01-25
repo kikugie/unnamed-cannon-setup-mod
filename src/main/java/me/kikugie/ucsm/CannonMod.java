f399609adf9319952e8dd9c6628c1f5f02dcb8d3
Kikugie <kikugie@duck.com>
2023-01-25 23:00:15 +0100

    Fixes:tm:

--- src/main/java/me/kikugie/ucsm/CannonMod.java
+++ src/main/java/me/kikugie/ucsm/CannonMod.java
@@ -2,45 +2,41 @@ package me.kikugie.ucsm;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.math.Vec3d;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 public class CannonMod implements ModInitializer {
     public static final Logger LOGGER = Logger.getLogger("ucsm");
-    public static Map<Vec3d, String> cannonData = new HashMap<>();
+    public static Vec3d[] points;
+    public static String[] configs;
     @Override
     public void onInitialize() {
         ClientCommandRegistrationCallback.EVENT.register(Command::register);
-        try {
-            initConfig();
-        } catch (FileNotFoundException e) {
-
-        }
+        ClientPlayConnectionEvents.JOIN.register(((handler, sender, client) -> initConfig()));
+        initConfig();
     }
 
-    public static void initConfig() throws FileNotFoundException {
+    public static boolean initConfig() {
         var configDir = new File(MinecraftClient.getInstance().runDirectory, "config/ucsm/");
         if (!configDir.exists()) {
             configDir.mkdirs();
         }
         try {
-            var points = readList(new File(configDir, "Pt.txt"));
-            var setups = readList(new File(configDir, "Ct.txt"));
-
-            cannonData.clear();
-            for (int i = 0; i < points.length; i++) {
-                var k = points[i].split(",");
-                var v = setups[i];
-                cannonData.put(new Vec3d(Float.parseFloat(k[0]), Float.parseFloat(k[1]), Float.parseFloat(k[2])), v);
-            }
+            points = Arrays.stream(readList(new File(configDir, "Pt.txt"))).map(val -> {
+                var k = val.split(",");
+                return new Vec3d(Float.parseFloat(k[0]), Float.parseFloat(k[1]), Float.parseFloat(k[2]));
+            }).toArray(Vec3d[]::new);
+            configs = readList(new File(configDir, "Ct.txt"));
+            return true;
         } catch (IOException e) {
             LOGGER.warning("Error reading config files");
+            return false;
         }
     }
 
--- src/main/java/me/kikugie/ucsm/Command.java
+++ src/main/java/me/kikugie/ucsm/Command.java
@@ -13,14 +13,17 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3i;
+import net.minecraft.world.RaycastContext;
 
 import java.io.FileNotFoundException;
 
+import static me.kikugie.ucsm.CannonMod.configs;
+import static me.kikugie.ucsm.CannonMod.points;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
 public class Command {
-    private static float sqTntRange = 16f;
+    private static float sqTntRange = 400f;
     private static Vec3i origin = null;
     private static Direction direction = null;
 
@@ -29,17 +32,18 @@ public class Command {
                 .executes(Command::help)
                 .then(literal("reload")
                         .executes(Command::reload))
-                .then(literal("explosion_range")
+                .then(literal("precision")
                         .then(argument("range", FloatArgumentType.floatArg(0.1f, 9000f))))
-                        .executes(Command::setTntRange)
+                .executes(Command::setTntRange)
                 .then(literal("origin")
                         .executes(Command::originFromPlayerPos)
                         .then(argument("pos", BlockPosArgumentType.blockPos())
                                 .then(argument("direction", new DirectionArgumentType())
                                         .executes(Command::setOrigin))))
                 .then(literal("target")
+                        .executes(Command::raycastTarget)
                         .then(argument("pos", BlockPosArgumentType.blockPos())
-                                .executes(Command::setTarget))));
+                                .executes(Command::posTarget))));
     }
 
 
@@ -51,58 +55,92 @@ public class Command {
     private static int setTntRange(CommandContext<FabricClientCommandSource> context) {
         float range = context.getArgument("range", Float.class);
         sqTntRange = range * range;
-        return 0;
+        return 1;
     }
 
     private static int reload(CommandContext<FabricClientCommandSource> context) {
         origin = null;
         direction = null;
-        try {
-            CannonMod.initConfig();
+        if (CannonMod.initConfig()) {
             context.getSource().sendFeedback(Text.of("§oConfig reloaded!"));
-        } catch (FileNotFoundException e) {
-            context.getSource().sendError(Text.of("Error reading config!"));
+            return 1;
         }
-        return 0;
+        context.getSource().sendError(Text.of("Error reading config!"));
+        return 1;
     }
 
-    private static int setTarget(CommandContext<FabricClientCommandSource> context) {
-        if (CannonMod.cannonData == null) {
-            context.getSource().sendError(Text.of("Config is not loaded!"));
-            return 1;
+    private static int raycastTarget(CommandContext<FabricClientCommandSource> context) {
+        var src = context.getSource();
+        var player = src.getPlayer();
+        BlockPos result = player.world.raycast(
+                new RaycastContext(
+                        player.getEyePos(),
+                        player.getEyePos().
+                                add(player.getRotationVecClient().
+                                        multiply(1000f)),
+                        RaycastContext.ShapeType.OUTLINE,
+                        RaycastContext.FluidHandling.ANY,
+                        player)
+        ).getBlockPos();
+
+        if (result.getSquaredDistance(player.getEyePos()) >= 990 * 990) {
+            context.getSource().sendError(Text.of("You are looking into nowhere..."));
+            return -1;
+        }
+
+        return getTarget(result, src) ? 1 : -1;
+    }
+
+    private static int posTarget(CommandContext<FabricClientCommandSource> context) {
+        var src = context.getSource();
+        return getTarget(getPosFromArgument(context.getArgument("pos", DefaultPosArgument.class), src), src) ? 1 : -1;
+    }
+
+    private static boolean getTarget(BlockPos pos, FabricClientCommandSource source) {
+        if (points == null || configs == null) {
+            source.sendError(Text.of("Config is not loaded!"));
+            return false;
         }
         if (origin == null || direction == null) {
-            context.getSource().sendError(Text.of("Origin is not set up!"));
-            return 1;
+            source.sendError(Text.of("Origin is not set up!"));
+            return false;
         }
 
-        Vec3d target = Vec3d.of(getPosFromArgument(context.getArgument("pos", DefaultPosArgument.class), context.getSource()))
-                .add(0.5d, 0.5d, 0.5d)
-                .subtract(origin.getX(), origin.getY(), origin.getZ())
-                .rotateY(-direction.asRotation());
+        // God forgive me
+        int rotate = switch (direction) {
+            case SOUTH -> 180;
+            case EAST -> 90;
+            case WEST -> -90;
+            default -> 0;
+        };
 
-        double squaredDistance = sqTntRange;
+        Vec3d target = Vec3d.of(pos)
+                .subtract(origin.getX() - 0.5, origin.getY() - 0.5, origin.getZ() - 0.5)
+                .rotateY(rotate);
+        double requiredDistance = sqTntRange;
         String configuration = null;
-        for (Vec3d pos : CannonMod.cannonData.keySet()) {
-            var distance = target.squaredDistanceTo(pos);
-            if (distance >= squaredDistance) {
-                configuration = CannonMod.cannonData.get(pos);
-                squaredDistance = distance;
+
+        for (int i = 0; i < CannonMod.points.length; i++) {
+            var distance = target.squaredDistanceTo(points[i]);
+            if (distance < requiredDistance) {
+                configuration = configs[i];
+                requiredDistance = distance;
             }
         }
+
         if (configuration == null) {
-            context.getSource().sendError(Text.of("Your target is over 9000 away!")); // ITS OVER 9000 is intentional
-            return 1;
+            source.sendError(Text.of("Your target is too far away!"));
+            return false;
         }
-        context.getSource().sendFeedback(Text.of("§aConfiguration: " + configuration + ", distance: " + Math.sqrt(squaredDistance)));
-        return 0;
+        source.sendFeedback(Text.of(String.format("§aConfiguration: %s; distance: %.2f", configuration, Math.sqrt(requiredDistance))));
+        return true;
     }
 
     private static int setOrigin(CommandContext<FabricClientCommandSource> context) {
         origin = getPosFromArgument(context.getArgument("pos", DefaultPosArgument.class), context.getSource());
         direction = Direction.byName(context.getArgument("direction", String.class));
         context.getSource().sendFeedback(Text.of("§oOrigin set to " + origin.toShortString() + " facing " + direction.asString()));
-        return 0;
+        return 1;
     }
 
     private static int originFromPlayerPos(CommandContext<FabricClientCommandSource> context) {
@@ -110,14 +148,16 @@ public class Command {
         origin = player.getBlockPos();
         direction = player.getHorizontalFacing();
         context.getSource().sendFeedback(Text.of("§oOrigin set to " + origin.toShortString() + " facing " + direction.asString()));
-        return 0;
+        return 1;
     }
 
     private static BlockPos getPosFromArgument(DefaultPosArgument argument, FabricClientCommandSource source) {
-
         DefaultPosArgumentAccessor accessor = (DefaultPosArgumentAccessor) argument;
         Vec3d pos = source.getPlayer().getPos();
-
-        return new BlockPos(accessor.getX().toAbsoluteCoordinate(pos.x), accessor.getY().toAbsoluteCoordinate(pos.y), accessor.getZ().toAbsoluteCoordinate(pos.z));
+        return new BlockPos(
+                accessor.getX().toAbsoluteCoordinate(pos.x),
+                accessor.getY().toAbsoluteCoordinate(pos.y),
+                accessor.getZ().toAbsoluteCoordinate(pos.z)
+        );
     }
 }
